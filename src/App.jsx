d0c825b3945b904273933dc99a02a6878f9decca
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-21 20:30:39 -0300

    fix counts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [count1, setCount1] = useState(0)
   return (
     <>
       <p>Sumar +2</p>
       <button onClick={() => setCount((count) => count + 2)}>{count} </button>
       <p>Sumar +3</p>
-      <button onClick={() => setCount((count) => count + 3)}>{count} </button>
+      <button onClick={() => setCount1((count1) => count1 + 3)}>{count1} </button>
     </>
   )
 }
