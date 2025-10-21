dbe264e0a1afb83c9c820b98e4366057a4bad313
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-21 20:48:01 -0300

    add div

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import './Style.css'
 function App() {
   const [count, setCount] = useState(0)
   const [count1, setCount1] = useState(0)
-  const [color, setColor] = useState('green, blue, red')
+  const [color, setColor] = useState('green')
+  const cambiarColor = () => {
+    if (color === 'green') setColor('blue')
+    else if (color === 'blue') setColor('red')
+    else setColor('green')
+  }
+
   return (
     <>
       <p>Sumar +2</p>
       <button onClick={() => setCount((count) => count + 2)}>{count} </button>
       <p>Sumar +3</p>
       <button onClick={() => setCount1((count1) => count1 + 3)}>{count1} </button>
-      <button onClick={() => setColor((color))}>Cambiar color</button>
-      <div className='square'>.</div>      
+      <button onClick={cambiarColor}>Cambiar color</button>
+      <div style={{
+        width: 100,
+        height: 100,
+        backgroundColor: color,
+        marginTop: 10
+      }}></div>
     </>
   )
 }
